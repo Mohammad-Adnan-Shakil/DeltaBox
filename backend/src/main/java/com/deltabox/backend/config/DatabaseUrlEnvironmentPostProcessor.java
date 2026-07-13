cec1c25e7c12175fe43e15d6bb7c12d745f8b2ca
Mohammad Adnan Shakil <muhammedadnsnahkil456@gmail.com>
2026-07-13 08:38:21 +0530

    chore: remove DB debug logging, disable Flyway post-schema-init

--- backend/src/main/java/com/deltabox/backend/config/DatabaseUrlEnvironmentPostProcessor.java
+++ backend/src/main/java/com/deltabox/backend/config/DatabaseUrlEnvironmentPostProcessor.java
@@ -34,11 +34,6 @@ public class DatabaseUrlEnvironmentPostProcessor implements EnvironmentPostProce
             return;
         }
 
-        System.err.println("=== DELTABOX DB DEBUG ===");
-        System.err.println("Final URL: " + normalized.jdbcUrl());
-        System.err.println("Username: " + normalized.username());
-        System.err.println("=========================");
-
         Map<String, Object> properties = new HashMap<>();
         properties.put("spring.datasource.url", normalized.jdbcUrl());
 
